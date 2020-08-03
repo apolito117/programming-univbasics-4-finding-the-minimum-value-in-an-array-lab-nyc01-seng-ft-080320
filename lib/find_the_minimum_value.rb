7907cd424502192913a72bf141dc41c9cc011f1e
Anthony Polito <anthonyalexanderpolito@gmail.com>
2020-08-03 22:17:56 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,3 +1,13 @@
 def find_min_value(array)
-   puts array.min
+  count = 0
+  min_value = -1
+  while count < array.length do
+    if min_value > array[count]
+      min_value = array[count]
+    end
+    count += 1
+  end
+  min_value
 end
+
+find_min_value ([2,3,4,5])
